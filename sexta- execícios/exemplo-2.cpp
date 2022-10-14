99f629aea220b61444d1cc5b8f6ca5d968b79a13
Amandacorreia1 <115305715+Amandacorreia1@users.noreply.github.com>
2022-10-14 16:59:42 -0300

    Resolução dos exemplos.

---
+++ sexta- execícios/exemplo-2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	
+	int pessoaa, pessoab;
+	
+	printf(" Digite a idade: ");
+	scanf("%d", &pessoaa);
+	
+	printf(" Digite a idade: ");
+	scanf("%d", &pessoab);
+	
+	if ((pessoaa % 2 !=0 ) || (pessoab % 2 !=0)){
+	
+			printf("Desconto");
+				
+		
+	}
+	
+	return 0;
+}
